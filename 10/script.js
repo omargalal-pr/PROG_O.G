fb183f3505bbb161cc4ffde963bdf0f415d6f249
Prog_O.G <Omar18galal@gmail.com>
2025-07-03 23:12:25 +0300

    Add files via upload

---
+++ 10/script.js
@@ -0,0 +1,9 @@
+for (let i = 0; i < 100; i++) {
+    const star = document.createElement("div");
+    star.className = "star";
+    star.style.top = Math.random() * 100 + "vh";
+    star.style.left = Math.random() * 100 + "vw";
+    star.style.animationDuration = (1 + Math.random() * 2) + "s";
+    star.style.opacity = Math.random();
+    document.body.appendChild(star);
+}
